39ec19b0d4dc130e3bda7805a90a86e1915886b1
jojolly6 <shorj5079@wrdsb.ca>
2021-02-18 16:49:24 +0000

    Completed the name and age input device

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * The Hello World Program
+ * @author Joanna Shorinde 
  */
 public class Main {
 
@@ -9,7 +11,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Ask the user for their name
+    System.out.println("Please enter your name:");
+
+    // Creating a Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // Declare and initialize the name variable
+    String name = input.nextLine();
+
+    // Say hello to the user
+    System.out.println("Hello " + name);
+
+    // Ask the user when they were born
+    System.out.println("Please enter the year you were born:");
+
+    // Create the variable to store the birth year
+    int birthYear = input.nextInt();
+
+    // Calculate the approximate age 
+    final int CURRENT_YEAR = 2021;
+    int age = CURRENT_YEAR - birthYear;
+
+    // Tell the user their age
+    System.out.println ("You are approximately " + age + " years old.");
     
   }
 }
